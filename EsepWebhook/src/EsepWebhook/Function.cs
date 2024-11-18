e1950eca44ccac23d1f41fe9ed8c5ebf17470b68
njblain1 <natalie.blain@ufl.edu>
2024-11-18 17:58:05 -0500

    took off end of link

--- EsepWebhook/src/EsepWebhook/Function.cs
+++ EsepWebhook/src/EsepWebhook/Function.cs
@@ -24,7 +24,7 @@ public class Function
         string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
         
         var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("https://s9qkmaaxwf.execute-api.us-east-2.amazonaws.com/default/EsepWebhook"))
+        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("https://s9qkmaaxwf.execute-api.us-east-2.amazonaws.com/default"))
         {
             Content = new StringContent(payload, Encoding.UTF8, "application/json")
         };
